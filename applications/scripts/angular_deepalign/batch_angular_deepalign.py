7d41777d4fe503126d2a900994a143d5c5215a79
Amaya Jimenez <ajimoreno@gmail.com>
2018-10-09 17:38:33 +0200

    More progress

--- applications/scripts/angular_deepalign/batch_angular_deepalign.py
+++ applications/scripts/angular_deepalign/batch_angular_deepalign.py
@@ -1,4 +1,4 @@
-#!/usr//bin/env python2
+#!/usr/bin/env python2
 
 from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.models import Model
@@ -15,8 +15,10 @@ import sys
 import xmippLib
 import time
 from keras.models import load_model
+from time import time
 
 batch_size = 128 # Number of boxes per batch
+
 
 def generateData(fnXmd, maxShift, maxPsi, mode, Nrepeats=30):
     mdIn = xmippLib.MetaData(fnXmd)
@@ -24,7 +26,7 @@ def generateData(fnXmd, maxShift, maxPsi, mode, Nrepeats=30):
     Y=None
     Nimgs = mdIn.size()
     idx = 0
-    keepAngle = mode=="psi"
+
     for objId in mdIn:
         fnImg = mdIn.getValue(xmippLib.MDL_IMAGE,objId)
         I = xmippLib.Image(fnImg)
@@ -32,55 +34,36 @@ def generateData(fnXmd, maxShift, maxPsi, mode, Nrepeats=30):
             Xdim, Ydim, _, _ = I.getDimensions()
             X = np.zeros((Nimgs*Nrepeats,Xdim,Ydim,1),dtype=np.float64)
             Y = np.zeros((Nimgs*Nrepeats,2),dtype=np.float64)
+            Xdim2=Xdim/2
+            Ydim2=Ydim/2
 
         for i in range(Nrepeats):
             psi = np.random.uniform(-maxPsi,maxPsi)*math.pi/180.0
             deltaX = np.random.uniform(-maxShift,maxShift)
             deltaY = np.random.uniform(-maxShift,maxShift)
-
             c = math.cos(psi)
             s = math.sin(psi)
-            M = np.float32([[c,s,deltaX],[-s,c,deltaY]])
-            X[idx,:,:,0] = cv2.warpAffine(I.getData(),M,(Xdim,Ydim))
+            M = np.float32([[c,s,(1-c)*Xdim2-s*Ydim2+deltaX],[-s,c,s*Xdim2+(1-c)*Ydim2+deltaY]])
+            X[idx,:,:,0] = cv2.warpAffine(I.getData(),M,(Xdim,Ydim)) #+ np.random.normal(0.0, 10.0, [Xdim, Xdim])
 
-            if keepAngle:
+            if mode=="psi":
                 Y[idx,0]=c
                 Y[idx,1]=s
-            else:
+            elif mode=="shift":
                 Y[idx,0]=deltaX
                 Y[idx,1]=deltaY
+            elif mode=="rot":
+		rot = mdIn.getValue(xmippLib.MDL_ANGLE_ROT,objId)
+		rot = np.deg2rad(rot)
+                Y[idx,0] = math.cos(rot)
+                Y[idx,1] = math.sin(rot)
+            elif mode=="tilt":
+		tilt = mdIn.getValue(xmippLib.MDL_ANGLE_TILT,objId)
+		tilt = np.deg2rad(tilt)
+                Y[idx,0] = math.cos(tilt)
+                Y[idx,1] = math.sin(tilt)
             idx+=1
-	print(X.shape)
     return X,Y,Xdim
-
-
-def loadData(fnXmd, maxShift, maxPsi):
-    mdIn = xmippLib.MetaData(fnXmd)
-    X=None
-    Nimgs = mdIn.size()
-    idx = 0
-    for objId in mdIn:
-        fnImg = mdIn.getValue(xmippLib.MDL_IMAGE,objId)
-        I = xmippLib.Image(fnImg)
-        if X is None:
-            Xdim, Ydim, _, _ = I.getDimensions()
-            X = np.zeros((Nimgs,Xdim,Ydim,1),dtype=np.float64)
-        psi = np.random.uniform(-maxPsi,maxPsi)*math.pi/180.0
-        deltaX = np.random.uniform(-maxShift,maxShift)
-        deltaY = np.random.uniform(-maxShift,maxShift)
-
-        c = math.cos(psi)
-        s = math.sin(psi)
-        M = np.float32([[c,s,deltaX],[-s,c,deltaY]])
-	if (idx==0):
-	    print(M.shape, I.getData().shape, Xdim, Ydim)
-        X[idx,:,:,0] = cv2.warpAffine(I.getData(),M,(Xdim,Ydim))
-	print(X.shape)
-        idx+=1
-        print("Ground Truth: c=",c," s=",s," deltaX=",deltaX," deltaY=", deltaY)
-    return X, Xdim
-
-
 
 def constructModel(Xdim):
     inputLayer = Input(shape=(Xdim,Xdim,1), name="input")
@@ -102,41 +85,26 @@ if __name__=="__main__":
     mode = sys.argv[4]
     fnODir = sys.argv[5]
     modelFn = sys.argv[6]
-    train = sys.argv[7]
 
-    if (train=="train"):
-        print('Train mode')
-        X, Y, Xdim = generateData(fnXmd, maxShift, maxPsi, mode)
-        model = constructModel(Xdim)
-        model.summary()
-        optimizer = Adam(lr=0.0001)
-        model.compile(loss='mean_absolute_error', optimizer='Adam')
-        history = model.fit(X, Y, batch_size=256, epochs=2, verbose=1, validation_split=0.1) #epochs=15
-        myValLoss=np.zeros((1))
-        myValLoss[0] = history.history['val_loss'][-1]
-        np.savetxt(os.path.join(fnODir,modelFn+'.txt'), myValLoss)
-        model.save(os.path.join(fnODir,modelFn+'.h5'))
-    else:
-        print('Predict mode')
-	X, Xdim = loadData(fnXmd, maxShift, maxPsi)
-        #model = load_model(os.path.join(fnODir,modelFn+'.h5'))
-	model = load_model('/home/ajimenez/ScipionUserData/projects/testDeepAlignment/Runs/000152_XmippProtDeepAlignment3D/extra/psi_iter000000.h5')
-        #loss, acc = model.evaluate(X, Y)
-        #print('loss=', loss, ' acc=', acc)
-        Ypred = model.predict(X)
-	print("Ypred psi ", Ypred)
-        M = np.float32([[Ypred[0,0],-Ypred[0,1],0],[Ypred[0,1],Ypred[0,0],0]])
-	print(M.shape, X.shape, Xdim)
-	print(X.shape)
-        newX = cv2.warpAffine(X,M,(Xdim,Xdim))
+    print('Train mode')
+    start_time = time()
+    X, Y, Xdim = generateData(fnXmd, maxShift, maxPsi, mode)
+    elapsed_time = time() - start_time
+    print("Time in generateData: %0.10f seconds." % elapsed_time)
+    start_time = time()
+    model = constructModel(Xdim)
+    model.summary()
+    optimizer = Adam(lr=0.0001)
+    model.compile(loss='mean_absolute_error', optimizer='Adam')
+    history = model.fit(X, Y, batch_size=256, epochs=2, verbose=1, validation_split=0.1)
+    myValLoss=np.zeros((1))
+    myValLoss[0] = history.history['val_loss'][-1]
+    np.savetxt(os.path.join(fnODir,modelFn+'.txt'), myValLoss)
+    model.save(os.path.join(fnODir,modelFn+'.h5'))
+    elapsed_time = time() - start_time
+    print("Time in training model: %0.10f seconds." % elapsed_time)
 
-	model = load_model('/home/ajimenez/ScipionUserData/projects/testDeepAlignment/Runs/000152_XmippProtDeepAlignment3D/extra/shift_iter000000.h5')
-        #loss, acc = model.evaluate(X, Y)
-        #print('loss=', loss, ' acc=', acc)
-        Ypred = model.predict(newX)
-	print("Ypred shift ", Ypred)
-        M = np.float32([[1.0,0.0,Ypred[0,0]],[0.0,1.0,Ypred[0,1]]])
-        newX = cv2.warpAffine(newX,M,(Xdim,Xdim))
+
 
 
 
---
+++ applications/scripts/angular_deepalign_predict/batch_angular_deepalign_predict.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python2
+
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten, Dense
+from keras.optimizers import Adam
+import tensorflow as tf
+import cv2
+import skimage
+import math
+import numpy as np
+import os
+import random
+import string
+import sys
+import xmippLib
+import time
+from keras.models import load_model
+#import pyworkflow.em.metadata as md
+
+batch_size = 128 # Number of boxes per batch
+
+
+
+def loadData(mdIn, fnXmd, maxShift, maxPsi):
+    X=None
+    Nimgs = mdIn.size()
+    newImage = xmippLib.Image()
+    idx = 0
+    for objId in mdIn:
+        fnImg = mdIn.getValue(xmippLib.MDL_IMAGE,objId)
+        I = xmippLib.Image(fnImg)
+        if X is None:
+            Xdim, Ydim, _, _ = I.getDimensions()
+#	    Xdim2=Xdim/2
+#            Ydim2=Ydim/2
+            X = np.zeros((Nimgs,Xdim,Ydim,1),dtype=np.float64)
+#	psiDeg = np.random.uniform(-maxPsi,maxPsi)
+#        psi = psiDeg*math.pi/180.0
+#        deltaX = np.random.uniform(-maxShift,maxShift)
+#        deltaY = np.random.uniform(-maxShift,maxShift)
+
+	# Saving data in the MD just for debugging purposes
+#	mdIn.setValue(xmippLib.MDL_ANGLE_PSI, float(psiDeg), objId)
+#	mdIn.setValue(xmippLib.MDL_SHIFT_X, float(deltaX), objId)
+#	mdIn.setValue(xmippLib.MDL_SHIFT_Y, float(deltaY), objId)
+
+#        c = math.cos(psi)
+#        s = math.sin(psi)
+#        M = np.float32([[c,s,(1-c)*Xdim2-s*Ydim2+deltaX],[-s,c,s*Xdim2+(1-c)*Ydim2+deltaY]])
+#        myImage = cv2.warpAffine(I.getData(),M,(Xdim,Ydim)) #+ np.random.normal(0.0, 10.0, [Xdim, Xdim])
+#	X[idx,:,:,0] = myImage
+#	newImage.setData(myImage)
+#	strFnOut = ('%06d@'+fnODir)%(idx+1)
+#	newImage.write(os.path.join(strFnOut,'inputImages.stk'))
+
+	X[idx,:,:,0] = I.getData()
+        idx+=1
+#        print("Ground Truth: psiDeg=", psiDeg," c=",c," s=",s," deltaX=",deltaX," deltaY=", deltaY)
+#    mdIn.write(fnXmd)
+    return X, Xdim, Ydim, Nimgs
+
+
+def matrix2str(M):
+    mystr = '[['+str(M[0,0])+','+str(M[0,1])+','+str(M[0,2])+']['+str(M[1,0])+','+str(M[1,1])+','+str(M[1,2])+']]'
+    return mystr
+
+def str2matrix(mystr):
+    mystrCp=''
+    for i in range(len(mystr)):
+        if mystr[i]=='[' or mystr[i]==']' or mystr[i]==',':
+	    mystrCp = mystrCp + ' '
+        else:
+	    mystrCp = mystrCp + mystr[i]
+    mylist = mystrCp.split(' ')	    	
+    myListNum = []
+    for num in mylist:
+        if num!='':
+            myListNum.append(float(num))
+    if len(myListNum)==9:
+    	myListNum = np.asarray(myListNum[:-3])
+    elif len(myListNum)==6:
+	myListNum = np.asarray(myListNum)
+    M = myListNum.reshape((2,3))
+    return M
+
+
+if __name__=="__main__":
+    fnXmd = sys.argv[1]
+    maxShift = float(sys.argv[2])
+    maxPsi = float(sys.argv[3])
+    fnODir = sys.argv[4]
+    numIter = int(sys.argv[5])
+
+    print('Predict mode')
+    newImage = xmippLib.Image()
+    mdIn = xmippLib.MetaData(fnXmd)
+    X, Xdim, Ydim, Ndim = loadData(mdIn, fnXmd, maxShift, maxPsi)
+
+    Xdim2=Xdim/2
+    Ydim2=Ydim/2
+    oneXprev = np.zeros((1,Xdim,Ydim,1),dtype=np.float64)
+    MIdentity = np.float32([[1.0,0.0,0.0],[0.0,1.0,0.0]])
+
+    for i in range(numIter*2):
+
+	if i%2==0:
+	    #We are in psi prediction
+            #model = load_model('/home/ajimenez/ScipionUserData/projects/testDeepAlignment/Runs/000152_XmippProtDeepAlignment3D/extra/psi_iter%06d.h5'%(i//2))
+	    #print(os.path.join(fnODir,'psi_iter%06d.h5'%(i//2)))
+	    model = load_model(os.path.join(fnODir,'psi_iter%06d.h5'%(i//2)))
+	else:
+	    #We are in shift prediction
+            #model = load_model('/home/ajimenez/ScipionUserData/projects/testDeepAlignment/Runs/000152_XmippProtDeepAlignment3D/extra/shift_iter%06d.h5'%(i//2))
+	    #print(os.path.join(fnODir,'shift_iter%06d.h5'%(i//2)))
+	    model = load_model(os.path.join(fnODir,'shift_iter%06d.h5'%(i//2)))
+	
+	idx=0
+	for objId in mdIn:
+	    if i==0:
+		mdIn.setValue(xmippLib.MDL_TRANSFORM_MATRIX, matrix2str(MIdentity), objId) #MDL_TRANSFORM_MATRIX with matrix for MD
+		mdIn.setValue(xmippLib.MDL_COMMENT, matrix2str(MIdentity), objId) #MDL_COMMENT with matrix for centered transformations with warpAffine
+	    oneX = X[idx,:,:,0]
+	    Mstr = mdIn.getValue(xmippLib.MDL_COMMENT, objId)
+	    #print(i,idx,Mstr)
+	    Mprev = str2matrix(Mstr)
+            oneXprev[0,:,:,0] = cv2.warpAffine(oneX,Mprev,(Xdim,Ydim))
+	    Ypred = model.predict(oneXprev)
+	    #print(i,idx,Ypred)
+	    if i%2==0:
+	        #We are in psi prediction
+	        c = Ypred[0,0]
+	        s = -Ypred[0,1]
+		psi = np.arctan2(s,c)
+		c = math.cos(psi)
+		s = math.sin(psi)
+	        Mnew = np.float32([[c,s,(1-c)*Xdim2-s*Ydim2],[-s,c,s*Xdim2+(1-c)*Ydim2]])
+		Mnew_MD = np.float32([[c,s,0.0],[-s,c,0.0]])
+	    else:
+	        #We are in shift prediction
+	        deltaX = -Ypred[0,0]
+	        deltaY = -Ypred[0,1]
+	        Mnew = np.float32([[1.0,0.0,deltaX],[0.0,1.0,deltaY]])
+		Mnew_MD = Mnew #np.float32([[1.0,0.0,Ypred[0,0]],[0.0,1.0,Ypred[0,1]]])
+	    #Saving matrix in MDL_COMMENT for centered transformations with warpAffine
+	    M = np.dot(np.vstack((Mnew,[0.0, 0.0, 1.0])), np.vstack((Mprev,[0.0, 0.0, 1.0])))
+	    mdIn.setValue(xmippLib.MDL_COMMENT, matrix2str(M), objId)
+	    #Saving matrix in MDL_TRANSFORM_MATRIX for MD
+	    Mstr_MD = mdIn.getValue(xmippLib.MDL_TRANSFORM_MATRIX, objId)
+	    Mprev_MD = str2matrix(Mstr_MD)
+	    M_MD = np.dot(np.vstack((Mnew_MD,[0.0, 0.0, 1.0])), np.vstack((Mprev_MD,[0.0, 0.0, 1.0])))
+	    mdIn.setValue(xmippLib.MDL_TRANSFORM_MATRIX, matrix2str(M_MD), objId)
+
+	    if i==((numIter*2)-1):
+		Mstr = mdIn.getValue(xmippLib.MDL_COMMENT, objId)
+	        Mdef = str2matrix(Mstr)
+                Xdef = cv2.warpAffine(oneX,Mdef,(Xdim,Ydim))
+	        newImage.setData(Xdef)
+	        strFnOut = ('%06d@'+fnODir)%(idx+1)
+	        newImage.write(os.path.join(strFnOut,'outputImages.stk'))
+		#mdIn.setValue(xmippLib.MDL_IMAGE, os.path.join(strFnOut,'outputImages.stk'), objId)
+
+		Mstr2 = mdIn.getValue(xmippLib.MDL_TRANSFORM_MATRIX, objId)
+	        Mdef2 = str2matrix(Mstr2)
+		psi = np.rad2deg(np.arctan2(Mdef2[0,1],Mdef2[0,0]))
+		mdIn.setValue(xmippLib.MDL_ANGLE_PSI, float(-psi), objId)
+		mdIn.setValue(xmippLib.MDL_SHIFT_X, float(Mdef2[0,2]), objId)
+		mdIn.setValue(xmippLib.MDL_SHIFT_Y, float(Mdef2[1,2]), objId)	
+
+	    idx+=1
+	mdIn.write(fnXmd)
+
+
+    #Checking if we have the models for 3D reconstruction
+    mdIn = xmippLib.MetaData(fnXmd)
+    if os.path.exists(os.path.join(fnODir,'rot_iter%06d.h5'%(numIter))):
+        model = load_model(os.path.join(fnODir,'rot_iter%06d.h5'%(numIter)))
+	idx=0
+	for objId in mdIn:
+	    oneX = X[idx,:,:,0]
+	    Mstr = mdIn.getValue(xmippLib.MDL_COMMENT, objId)
+	    Mprev = str2matrix(Mstr)
+            oneXprev[0,:,:,0] = cv2.warpAffine(oneX,Mprev,(Xdim,Ydim))
+	    Ypred = model.predict(oneXprev)
+            c = Ypred[0,0]
+            s = Ypred[0,1]
+	    rot = np.rad2deg(np.arctan2(s,c))
+	    if rot<0:
+		rot=360+rot
+	    #print(i,idx,rot)
+	    mdIn.setValue(xmippLib.MDL_ANGLE_ROT, float(rot), objId)
+	    idx+=1
+	mdIn.write(fnXmd)
+
+    mdIn = xmippLib.MetaData(fnXmd)
+    if os.path.exists(os.path.join(fnODir,'tilt_iter%06d.h5'%(numIter))):
+        model = load_model(os.path.join(fnODir,'tilt_iter%06d.h5'%(numIter)))
+	idx=0
+	for objId in mdIn:
+	    oneX = X[idx,:,:,0]
+	    Mstr = mdIn.getValue(xmippLib.MDL_COMMENT, objId)
+	    Mprev = str2matrix(Mstr)
+            oneXprev[0,:,:,0] = cv2.warpAffine(oneX,Mprev,(Xdim,Ydim))
+	    Ypred = model.predict(oneXprev)
+            c = Ypred[0,0]
+            s = Ypred[0,1]
+	    tilt = np.rad2deg(np.arctan2(s,c))
+	    if tilt<0:
+		tilt=360+tilt
+	    #print(i,idx,tilt)
+	    mdIn.setValue(xmippLib.MDL_ANGLE_TILT, float(tilt), objId)
+	    idx+=1
+	mdIn.write(fnXmd)
+
+
+
+
